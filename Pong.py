4642b205697651fb023adb4406dc5780941c8e76
Alessandro <alessandrofasolilli@googlemail.com>
2019-07-19 14:05:22 +0100

    first commit

---
+++ Pong.py
@@ -0,0 +1,128 @@
+# Simple Pong game using turtle framework
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by Alessandro")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Score
+score_a = 0
+score_b = 0
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup() #stop lines being drawn when it comes
+paddle_a.goto(-350, 0) # horizontal , vertical
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup() #stop lines being drawn when it comes
+paddle_b.goto(350, 0) # horizontal , vertical
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white") 
+ball.penup() #stop lines being drawn when it comes
+ball.goto(0, 0) # horizontal , vertical
+ball.dx = .3 # This defines the number of pixels the ball moves by each time
+ball.dy = -.3
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+
+# Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20 # add 20 pixels to y coordinate
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20 # add 20 pixels to y coordinate
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20 # add 20 pixels to y coordinate
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20 # add 20 pixels to y coordinate
+    paddle_b.sety(y)
+
+
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+#Main game loop
+while True:
+    wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)    
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290: # Ball hits the top of screen
+        ball.sety(290)
+        ball.dy *= -1 # Reverse direction
+
+    if ball.ycor() < -290: # Ball hits the bottom of screen
+        ball.sety(-290)
+        ball.dy *= -1 # Reverse direction
+
+    if ball.xcor() > 390: # Ball has gone past the paddle
+        ball.goto(0,0)
+        ball.dx *= -1 # Reverse direction
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+    
+    if ball.xcor() < -390: # Ball has gone past the paddle
+        ball.goto(0,0)
+        ball.dx *= -1 # Reverse direction
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
+
+    # Paddle and ball collisions
+
+    # 340 is the start position of paddle b
+    # 40 accounts for the size of the ball
+    if (ball.xcor() > 340) and (ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1 # Reverse direction
+
+    # -340 is the start position of paddle b
+    # 40 accounts for the size of the ball
+    if (ball.xcor() < -340) and (ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1 # Reverse direction
+
